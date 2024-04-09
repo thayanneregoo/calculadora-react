adbe5df41ab97412f68212b30e748d75e47bf0ad
thayanneregoo <thayanneregoo@hotmail.com>
2024-04-08 21:10:47 -0300

    adicionando class no app.js

--- App.js
+++ App.js
@@ -1,15 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import Button from './components/Button'
+import Row from './components/Button'
+import calculator, {initalState} from './util/calculator';
+import { Component } from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Luana</Text>
-      <StatusBar style="auto" />
+
+export default class App extends Component{
+state= initalState;
+
+HandleTap =(type, value) =>{
+  this.setState((state) => calculator(type, value, state));
+};
+render(){
+  return(
+    <View style={style.container}>
+      <SafeAreaView>
+        <Text style={styles.value}>
+        {parseFloat(this.state.currentValue).toLocaleString()}
+        </Text>
+      </SafeAreaView>
     </View>
-  );
+  )
 }
 
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
